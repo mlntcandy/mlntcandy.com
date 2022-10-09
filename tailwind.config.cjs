ebf8cc677e08e7569408e958e8720aee496995da
mlntcandy <me@mlntcandy.com>
2022-10-09 15:26:23 +0300

    hello, astro!

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'primary': '#FF86DD',
+				'secondary': '#FFA48F',
+				'seastone': '#30393E',
+				'deepslate': '#222222',
+				'shallowslate': '#262829'
+			}
+		}
+	},
+	plugins: [],
+}
